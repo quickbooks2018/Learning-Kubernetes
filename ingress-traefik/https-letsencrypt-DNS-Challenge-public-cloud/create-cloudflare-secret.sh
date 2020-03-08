3c386dd2cfd9e9d857aad873b47da3a8e25cf9a1
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2020-03-08 07:19:25 +0500

    Update create-cloudflare-secret.sh

--- ingress-traefik/https-letsencrypt-DNS-Challenge-public-cloud/create-cloudflare-secret.sh
+++ ingress-traefik/https-letsencrypt-DNS-Challenge-public-cloud/create-cloudflare-secret.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 kubectl --namespace=kube-system create secret generic cloudflare-dns-secrets \
   --from-literal=CLOUDFLARE_EMAIL="your-email-address" \
-  --from-literal=CLOUDFLARE_API_KEY ="your-akikey-from-cloudflare"
+  --from-literal=CLOUDFLARE_API_KEY="your-akikey-from-cloudflare"
 
 #END
